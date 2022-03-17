91d5cee398ef06d4781df9cd156f7e59d1ee1d7d
Nacamino <nataliacam885@gmail.com>
2022-03-17 08:59:21 -0300

    backend validator new product

--- src/routes/productRutas.js
+++ src/routes/productRutas.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/productController')
-const upload = require('../middleware/middlemulter')
-const jerarquiaMiddleware = require('../middleware/jerarquiaMiddleware')
-const validationProductEdit = require('../middleware/validatorEditProductMiddleware');
+const productController = require("../controllers/productController");
+const upload = require("../middleware/middlemulter");
+const jerarquiaMiddleware = require("../middleware/jerarquiaMiddleware");
+const validationProductEdit = require("../middleware/validatorEditProductMiddleware");
+const validationNewProduct = require("../middleware/validatorNewProduct");
 
-
-router.get('/cart', productController.cart);
-router.get('/detailProduct/:id', productController.detail);
-router.get('/search', productController.search);
-router.get('/getProducts/:categoria',productController.getProducts);
+router.get("/cart", productController.cart);
+router.get("/detailProduct/:id", productController.detail);
+router.get("/search", productController.search);
+router.get("/getProducts/:categoria", productController.getProducts);
 
 //CRUD de Products
-router.get('/createProduct', jerarquiaMiddleware, productController.create);
-router.post('/storeProduct', upload.single("portada"), productController.store);
-router.get('/edit/:id', jerarquiaMiddleware ,productController.edit)
-router.put('/edit/:id',upload.single("image"),validationProductEdit,  productController.update)
-router.post("/delete/:id", productController.eliminar)
+router.get(
+  "/createProduct",
+  jerarquiaMiddleware,
+  upload.single("image"),
+  validationNewProduct,
+  productController.create
+);
+router.post("/storeProduct", upload.single("portada"), productController.store);
+router.get("/edit/:id", jerarquiaMiddleware, productController.edit);
+router.put(
+  "/edit/:id",
+  upload.single("image"),
+  validationProductEdit,
+  productController.update
+);
+router.post("/delete/:id", productController.eliminar);
 module.exports = router;
